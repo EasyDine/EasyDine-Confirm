6ec9eaa8e2db0689e8b8654c38ed3e5bc244b84a
AnnikDev <a_kvernadze@cu.edu.ge>
2024-03-10 18:47:42 +0400

    edit

--- src/components/reservation-info/Reservation-info.jsx
+++ src/components/reservation-info/Reservation-info.jsx
@@ -9,10 +9,7 @@ export default function ReservationInfo() {
                 <p className="personName">Bill Gates</p>
                 <svg className="infoIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none" /><circle cx="128" cy="128" r="96" fill="none" stroke="#2f372d" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" /><path d="M120,120a8,8,0,0,1,8,8v40a8,8,0,0,0,8,8" fill="none" stroke="#2f372d" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" /><circle cx="124" cy="84" r="12" /></svg>
             </div>
-
             <time dateTime="04/04/2024"> 04/04/2024</time>
-
-
             <div className="reservationInfo">
                 <ul>
                     <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none" /><rect x="64" y="32" width="128" height="40" rx="8" fill="none" stroke="#2f372d" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" /><line x1="168" y1="72" x2="192" y2="224" fill="none" stroke="#2f372d" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" /><line x1="64" y1="224" x2="88" y2="72" fill="none" stroke="#2f372d" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" /><line x1="72.84" y1="168" x2="183.16" y2="168" fill="none" stroke="#2f372d" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" /></svg>Table 4</li>
