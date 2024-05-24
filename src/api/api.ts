496451cf3a81f193de890360dd5563d18882cba6
Mikheil <84536014+mberrishdev@users.noreply.github.com>
2024-05-24 12:08:55 +0400

    update url

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 // import https from "https";
 
 const api = axios.create({
-  baseURL: "https://localhost:5000/api/v1",
+  baseURL: "https://easydine-api.azurewebsites.net/api/v1",
   headers: {
     "X-Channel": "ConfirmUi",
   },
